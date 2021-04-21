f2c2f38fc001e672de6074af64299babd5f9f491
khairul773 <mdkhairul105islam@gmail.com>
2021-04-21 06:50:30 +0600

    last update

--- js/app.js
+++ js/app.js
@@ -1,25 +1,13 @@
 $(document).ready(function(){
-	$('#navbar_open').on("click",function(event) {
-	    event.preventDefault();
-	   
-		var side_navbar = $('.side_navbar');
-		if(side_navbar.hasClass("active")) {
-			side_navbar.removeClass("active");
-		} else {
-			side_navbar.addClass("active");
-		}
-		
-		var wrapper = $('.wrapper_background');
-		if(wrapper.hasClass("wrapper_active")) {
-			wrapper.removeClass("wrapper_active");
-		} else {
-			wrapper.addClass("wrapper_active");
-		}
-	});
-	$(".wrapper_background").on("click", function() {
-		$('.side_navbar').removeClass("active");
-	});
-	$(".wrapper_background").on("click", function() {
-		$('.wrapper_background').removeClass("wrapper_active");
-	});
+	$(".dropdown_li .nav-link").on("click", function(event) {
+		var parent = $(this).closest(".dropdown_li");
+
+        if(parent.hasClass("active")) {
+            parent.removeClass("active");
+        } else {
+            parent.addClass("active");
+            $( ".dropdown_li" ).not( parent ).removeClass( "active" );
+        }
+        event.preventDefault();
+    });
 });
